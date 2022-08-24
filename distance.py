49ee3b59f1dd1e628aef27297b8db1967096bfed
gollabharadwaj <111568643+gollabharadwaj@users.noreply.github.com>
2022-08-24 22:26:50 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,6 @@
+x1=int(input("enter the value of x1 "))
+y1=int(input("enter the value of y1 "))
+x2=int(input("enter the value of x2 "))
+y2=int(input("enter the value of y2 "))
+distance=float((x2-x1)**2 + (y2-y1)**2)**0.5
+print("The distance between the coordinates is ",distance)
